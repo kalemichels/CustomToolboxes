2d7544d10d0b6db7f50bcb9cbb391fcf90d547c4
kalemichels <michelsk@live.com>
2023-04-15 12:10:33 -0500

    Initial Load of Minecraft Toolbox

---
+++ minecraftCustomToolbox/frmMinecraftCustomToolbox.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace minecraftCustomToolbox
+{
+    partial class frmMinecraftCustomToolbox
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMinecraftCustomToolbox));
+            btnNewOneblockWorld = new Button();
+            groupBox1 = new GroupBox();
+            textBox1 = new TextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnNewOneblockWorld
+            // 
+            btnNewOneblockWorld.BackColor = SystemColors.MenuHighlight;
+            btnNewOneblockWorld.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            btnNewOneblockWorld.Location = new Point(6, 24);
+            btnNewOneblockWorld.Name = "btnNewOneblockWorld";
+            btnNewOneblockWorld.Size = new Size(80, 80);
+            btnNewOneblockWorld.TabIndex = 0;
+            btnNewOneblockWorld.Text = "New Oneblock World";
+            btnNewOneblockWorld.UseVisualStyleBackColor = false;
+            btnNewOneblockWorld.Click += btnNewOneblockWorld_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.Tomato;
+            groupBox1.Controls.Add(btnNewOneblockWorld);
+            groupBox1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(289, 120);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Vanilla Tools";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(364, 236);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // frmMinecraftCustomToolbox
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(groupBox1);
+            Name = "frmMinecraftCustomToolbox";
+            Text = "Minecraft Toolbox";
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnNewOneblockWorld;
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+    }
+}
---
+++ minecraftCustomToolbox/frmMinecraftCustomToolbox.cs
@@ -0,0 +1,87 @@
+using System.CodeDom;
+
+namespace minecraftCustomToolbox
+{
+    public partial class frmMinecraftCustomToolbox : Form
+    {
+        public frmMinecraftCustomToolbox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewOneblockWorld_Click(object sender, EventArgs e)
+        {
+            var oneblockTemplate = Directory.GetFiles(
+                System.IO.Directory.GetDirectories(
+                    System.IO.Directory.GetCurrentDirectory())
+                .ToList().First(x => x.EndsWith("FileResources")))
+                .First(x => x.EndsWith("OneBlock1.19.zip"));
+            if (oneblockTemplate != null)
+            {
+                var newWorldName = TextMessageBox.Show("What would you like to call the new world?");
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.minecraft\\saves\\tmp\\");
+                //C:\Users\miche\AppData\Roaming\.minecraft\saves
+                if (newWorldName != null)
+                {
+                    System.IO.Compression.ZipFile.ExtractToDirectory(oneblockTemplate, Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.minecraft\\saves\\tmp\\");
+                    System.IO.Directory.Move(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.minecraft\\saves\\tmp\\OneBlock Reborn 1.19.3", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\.minecraft\\saves\\" + newWorldName);
+                }
+
+                int f = 0;
+            }
+        }
+    }
+
+    public partial class frmTextMessageBox : Form
+    {
+        public string response;
+        public frmTextMessageBox(string message)
+        {
+            Text = "Prompt";
+            Label lblOne = new Label();
+            lblOne.Text = message;
+            lblOne.Location = new System.Drawing.Point(5, this.Top + 5);
+            lblOne.Size = new System.Drawing.Size(300, 23);
+
+
+            TextBox txOne = new TextBox();
+            txOne.Location = new System.Drawing.Point(5, Top + 25);
+            txOne.Size = new System.Drawing.Size(100, 23);
+            txOne.TabIndex = 0;
+            txOne.TextChanged += UpdateResponse;
+
+            Button btnSubmit = new Button();
+            btnSubmit.Size = new System.Drawing.Size(40, 40);
+            btnSubmit.Location = new System.Drawing.Point(5, Bottom - btnSubmit.Size.Height - 50);
+            btnSubmit.Text = "Ok";
+            btnSubmit.Click += CloseForm;
+
+            Controls.Add(lblOne);
+            Controls.Add(txOne);
+            Controls.Add(btnSubmit);
+
+        }
+        public void UpdateResponse(object sender, EventArgs e)
+        {
+            response = ((TextBox)sender).Text;
+        }
+        public void CloseForm(object sender, EventArgs e)
+        {
+            var x = 0;
+            this.Close();
+        }
+
+    }
+    public static class TextMessageBox
+    {
+        public static string Show(string message)
+        {
+            using (var frmInstance = new frmTextMessageBox(message))
+            {
+                frmInstance.ShowDialog();
+                return frmInstance.response;
+            }
+        }
+    }
+
+}
